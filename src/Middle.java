3384846ce79d4928c0c383e6d500b9a1636f534a
HITtheFLOORT <1425087554@qq.com>
2021-09-15 09:01:44 +0800

    commit

--- src/Middle.java
+++ src/Middle.java
@@ -538,4 +538,29 @@ public class Middle {
         }
         return "";
     }
+    public int findPeakElement(int[] nums) {
+        int l=0;
+        int r=nums.length-1;
+        while(l<r){
+            int mid=(l+r)/2;
+            if(compare(nums,mid-1,mid)<0&&compare(nums,mid,mid+1)>0){
+                return mid;
+            }
+            if(compare(nums, mid,mid+1)<0){
+                l=mid+1;
+            }else{
+                r=mid-1;
+            }
+        }
+        return r;
+    }
+    public int compare(int []nums,int a,int b){
+        if(a<0){
+            return -1;
+        }else if(b>=nums.length){
+            return 1;
+        }else{
+            return nums[a]-nums[b];
+        }
+    }
 }
